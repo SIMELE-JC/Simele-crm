670bbb2c0f640ee66e3cb2eed08cb8f3de8560dc
SIMELE-JC <ccs.guadeloupe@outlook.fr>
2026-04-03 09:16:33 -0400

    Add crash logging

--- server/index.js
+++ server/index.js
@@ -1,3 +1,11 @@
+process.on('uncaughtException', (err) => {
+  console.error('💥 CRASH:', err.message);
+  console.error(err.stack);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.error('💥 REJECTION:', err);
+});
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
